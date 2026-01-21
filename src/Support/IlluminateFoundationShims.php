f82dced6389f3ba00b53dab2ea6eb1a596130f59
Dmytro Lukianenko <dmi3yy@gmail.com>
2026-01-22 00:54:02 +0100

    [FIX] IlluminateFoundationShims: Refactor namespace declarations to use braces for consistency

--- src/Support/IlluminateFoundationShims.php
+++ src/Support/IlluminateFoundationShims.php
@@ -1,55 +1,55 @@
 <?php
 
-namespace Illuminate\Foundation\Bus;
-
-if (!trait_exists(DispatchesJobs::class)) {
-    trait DispatchesJobs
-    {
-        public function dispatch($job)
+namespace Illuminate\Foundation\Bus {
+    if (!trait_exists(DispatchesJobs::class)) {
+        trait DispatchesJobs
         {
-            if (function_exists('app') && interface_exists(\Illuminate\Contracts\Bus\Dispatcher::class)) {
-                return app(\Illuminate\Contracts\Bus\Dispatcher::class)->dispatch($job);
+            public function dispatch($job)
+            {
+                if (function_exists('app') && interface_exists(\Illuminate\Contracts\Bus\Dispatcher::class)) {
+                    return app(\Illuminate\Contracts\Bus\Dispatcher::class)->dispatch($job);
+                }
+
+                throw new \RuntimeException('Bus dispatcher is not available.');
             }
 
-            throw new \RuntimeException('Bus dispatcher is not available.');
-        }
+            public function dispatchSync($job)
+            {
+                if (function_exists('app') && interface_exists(\Illuminate\Contracts\Bus\Dispatcher::class)) {
+                    return app(\Illuminate\Contracts\Bus\Dispatcher::class)->dispatchSync($job);
+                }
 
-        public function dispatchSync($job)
-        {
-            if (function_exists('app') && interface_exists(\Illuminate\Contracts\Bus\Dispatcher::class)) {
-                return app(\Illuminate\Contracts\Bus\Dispatcher::class)->dispatchSync($job);
+                return $this->dispatch($job);
             }
 
-            return $this->dispatch($job);
-        }
+            public function dispatchNow($job, $handler = null)
+            {
+                if (function_exists('app') && interface_exists(\Illuminate\Contracts\Bus\Dispatcher::class)) {
+                    return app(\Illuminate\Contracts\Bus\Dispatcher::class)->dispatchNow($job, $handler);
+                }
 
-        public function dispatchNow($job, $handler = null)
-        {
-            if (function_exists('app') && interface_exists(\Illuminate\Contracts\Bus\Dispatcher::class)) {
-                return app(\Illuminate\Contracts\Bus\Dispatcher::class)->dispatchNow($job, $handler);
+                return $this->dispatch($job);
             }
-
-            return $this->dispatch($job);
         }
     }
 }
 
-namespace Illuminate\Foundation\Validation;
-
-if (!trait_exists(ValidatesRequests::class)) {
-    trait ValidatesRequests
-    {
-        public function validate(\Illuminate\Http\Request $request, array $rules, array $messages = [], array $attributes = [])
+namespace Illuminate\Foundation\Validation {
+    if (!trait_exists(ValidatesRequests::class)) {
+        trait ValidatesRequests
         {
-            $validator = validator($request->all(), $rules, $messages, $attributes);
-            return $validator->validate();
-        }
+            public function validate(\Illuminate\Http\Request $request, array $rules, array $messages = [], array $attributes = [])
+            {
+                $validator = validator($request->all(), $rules, $messages, $attributes);
+                return $validator->validate();
+            }
 
-        public function validateWithBag(string $errorBag, \Illuminate\Http\Request $request, array $rules, array $messages = [], array $attributes = [])
-        {
-            $validator = validator($request->all(), $rules, $messages, $attributes);
-            $validator->setErrorBag($errorBag);
-            return $validator->validate();
+            public function validateWithBag(string $errorBag, \Illuminate\Http\Request $request, array $rules, array $messages = [], array $attributes = [])
+            {
+                $validator = validator($request->all(), $rules, $messages, $attributes);
+                $validator->setErrorBag($errorBag);
+                return $validator->validate();
+            }
         }
     }
 }
